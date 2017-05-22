a15dcadb3913e041fa5b2a2c17b5693c66e64a03
Marwan-G <ghabin2004@gmail.com>
2017-05-22 23:27:41 +0200

    setting up state

--- src/Popular.js
+++ src/Popular.js
@@ -3,17 +3,25 @@ class Popular extends React.Component{
 constructor(props){
   super(props);
   this.state={
-    selectedLaanguage: 'All'
-  }
+    selectedLanguage: 'All'
+  };
 }
+  updatedLanguage(lang){
+    this.setState(function(){
+      return {
+        selectedLanguage:lang
+      }
+    });
+  }
   render(){
     var languages = ['All', 'JavaScript','Ruby', 'Java', 'CSS', 'Python'];
+    console.log("up here ", this)
     return(
-
         <ul className="languages">
-          {languages.map((lang) => {
+            <p>selectedLanguage is {this.state.selectedLanguage}</p>
+          {languages.map((lang) => { console.log("down her", this)
             return(
-              <li key={lang}>{lang}</li>
+              <li key={lang} onClick={this.updatedLanguage.bind(this,lang)}>{lang}</li>
             )
             })
          }
